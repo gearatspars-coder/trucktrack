9bd2e37981ddb4d5ef93279217d59544433b2778
gearatspars-coder <gearat.spars@gmail.com>
2026-01-04 13:47:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Since files are in root, we don't need a custom root entry 
+  // unless your index.html points to a different path.
 })
